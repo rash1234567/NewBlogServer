b67528c26451d4801c80f161713e501c83b85ee6
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-11-09 05:13:37 -0800

    use cors

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(blogRoutes)
+app.use(cors())
 
 
 mongoose.connect(process.env.DB_URL).then(()=> {
